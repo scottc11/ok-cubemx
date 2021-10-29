c62718018e5fbcc1005b8b9990112468d95a2384
scottc11 <scott.m.campbell11@gmail.com>
2021-10-29 12:19:49 -0400

    moved gpio toggle into user code block

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -93,6 +93,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
+    HAL_Delay(100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
